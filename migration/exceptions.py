e72a04e89f5dfc03206ea723932f4ef652f57ca4
Santiago Olarte <saolarte@msn.com>
2025-05-14 03:44:34 -0500

    implement custom errors

---
+++ migration/exceptions.py
@@ -0,0 +1,6 @@
+class DuplicateDataError(Exception):
+    def __init__(self, message, duplicates, entity):
+        super().__init__()
+        self.message = message
+        self.duplicates = duplicates
+        self.entity = entity
