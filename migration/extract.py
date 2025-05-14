795020d71eea6f0451c5e5cb8415c8c7ae9e9d78
Santiago Olarte <saolarte@msn.com>
2025-05-14 00:05:54 -0500

    Improve local file loading

--- migration/extract.py
+++ migration/extract.py
@@ -1,11 +1,9 @@
+import os
 import pandas as pd
 
 
-def load_data_into_df(file_location, fields):
-    df = pd.read_csv(file_location, names=fields, header=None)
-    import pdb; pdb.set_trace()
-    print("hello!")
+def load_data_into_df(file_name, fields):
+    base_dir =  os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(base_dir, file_name)
+    df = pd.read_csv(file_path, names=fields, header=None)
     return df
-
-
-
