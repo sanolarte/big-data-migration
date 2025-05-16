4bfeeebe8d5bd691f57e33f30ee6c02f729f884e
sanolarte <san.9405@gmail.com>
2025-05-16 06:02:31 -0500

    Challenge 2 (#4)
    * Create endpoint to retrieve employees hired by quarter
    
    * add more than the mean handler and query

--- database/queries.py
+++ database/queries.py
@@ -29,3 +29,57 @@ def build_insert_query(entity, fields, foreign_keys):
         """
 
     return query_string
+
+
+def employees_by_quarter(year_of_hire):
+    query = f"""
+        SELECT
+            d.department_name AS department,
+            j.job_name AS job,
+            COUNT(CASE WHEN QUARTER(e.hire_datetime) = 1 THEN 1 END) AS q1,
+            COUNT(CASE WHEN QUARTER(e.hire_datetime) = 2 THEN 1 END) AS q2,
+            COUNT(CASE WHEN QUARTER(e.hire_datetime) = 3 THEN 1 END) AS q3,
+            COUNT(CASE WHEN QUARTER(e.hire_datetime) = 4 THEN 1 END) AS q4
+        FROM employees e
+        JOIN departments d ON e.department_id = d.id
+        JOIN jobs j ON e.job_id = j.id
+        WHERE YEAR(e.hire_datetime) = {year_of_hire}
+        GROUP BY
+            d.department_name,
+            j.job_name
+        ORDER BY
+            d.department_name ASC,
+            j.job_name ASC;
+
+
+        """
+    return query
+
+
+def more_than_the_mean_hired_employees(year):
+    query = f"""
+        WITH hires_by_department AS (
+            SELECT
+                d.department_id,
+                d.department_name,
+                COUNT(e.id) AS total_hires
+            FROM departments d
+            LEFT JOIN employees e ON e.department_id = d.id AND YEAR(e.hire_datetime) = {year}
+            GROUP BY d.department_id, d.department_name
+        ),
+        with_avg AS (
+            SELECT *,
+                AVG(total_hires) OVER () AS avg_hires
+            FROM hires_by_department
+        )
+        SELECT
+            department_id,
+            department_name,
+            total_hires
+        FROM with_avg
+        WHERE total_hires > avg_hires
+        ORDER BY total_hires DESC;
+
+        """
+
+    return query
